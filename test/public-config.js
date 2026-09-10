b5037af56016a4463368229f76873a612d117dfa
giorgioss123-dotcom <giorgioss123@gmail.com>
2026-09-10 23:34:39 +0200

    Add files via upload

---
+++ test/public-config.js
@@ -0,0 +1,10 @@
+window.KBK_APP_CONFIG = {
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwZ1Q8B3gcG5hoqJjAqsNG8WX85m0WpICv5h47H8YIqhmQvMVAlwXwsAgqn3tEHDYo6gw/exec'
+};
+
+window.KBK_PUBLIC_SHEET_IDS = {
+  cennikFileId: '1YwUfc_SrM9yAj4CasraaLqi32qEEIxTSs4CJyxlXXgY',
+  wnioskiFileId: '1mRTK0JR6D7qIE7aIn2ggEuu5QBTOhdYGqO0kD8PbtLU',
+  transakcjeFileId: '1dDVJd-BbCpnvbTDa0gacVRYeGzQ7zGHE5iM3NRhLMSw',
+  transakcjeArchiveFileId: '1SnhoLAv_X2lqlsVuhzghr2_2bDbLBIQS3ERxRS66xJg'
+};
