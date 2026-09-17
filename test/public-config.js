c2f4d4152172194f027d03cfe11d3db037971839
giorgioss123-dotcom <giorgioss123@gmail.com>
2026-09-18 00:10:35 +0200

    Add files via upload

--- test/public-config.js
+++ test/public-config.js
@@ -1,10 +1,10 @@
 window.KBK_APP_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbyke3jFISgc3r9R2xkT7Fz9q2fexxmcc9St4xrCKLj-zjEI5PStbmlDjF5VT-GN38aT_A/exec'
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbw8TLpdXJ4QBKkr9R5gpKz5KTJyfBMg5KsaCzbKMjzEygOpzAJoxP1eV7rjeO6_s9XuCw/exec'
 };
 
 window.KBK_PUBLIC_SHEET_IDS = {
-  cennikFileId: '1YwUfc_SrM9yAj4CasraaLqi32qEEIxTSs4CJyxlXXgY',
+  cennikFileId: '1hNjYyBCsWSEGOSqjnIGl6mYjldfz-qkxIfy73BMHd0M',
   wnioskiFileId: '1mRTK0JR6D7qIE7aIn2ggEuu5QBTOhdYGqO0kD8PbtLU',
   transakcjeFileId: '1dDVJd-BbCpnvbTDa0gacVRYeGzQ7zGHE5iM3NRhLMSw',
-  transakcjeArchiveFileId: '1SnhoLAv_X2lqlsVuhzghr2_2bDbLBIQS3ERxRS66xJg'
+  transakcjeArchiveFileId: ''
 };
