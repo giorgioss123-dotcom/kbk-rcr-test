85c0ca793f413ea7f0c380e4d05c711e574a1085
giorgioss123-dotcom <giorgioss123@gmail.com>
2026-09-24 22:55:01 +0200

    Update public-config.js

--- test/public-config.js
+++ test/public-config.js
@@ -6,5 +6,5 @@ window.KBK_PUBLIC_SHEET_IDS = {
   cennikFileId: '1YwUfc_SrM9yAj4CasraaLqi32qEEIxTSs4CJyxlXXgY',
   wnioskiFileId: '1mRTK0JR6D7qIE7aIn2ggEuu5QBTOhdYGqO0kD8PbtLU',
   transakcjeFileId: '1dDVJd-BbCpnvbTDa0gacVRYeGzQ7zGHE5iM3NRhLMSw',
-  transakcjeArchiveFileId: '1SnhoLAv_X2lqlsVuhzghr2_2bDbLBIQS3ERxRS66xJg'
+  transakcjeArchiveFileId: '1JmnImqQYLjNE_EaERhS-cYdcwljCGnxCKDYpyNYmE0I'
 };
